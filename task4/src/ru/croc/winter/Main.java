87f2b43e3ab7c7aaabb542d4579e6875f4c1ffad
sadnastya <alwaysbebetter@mail.ru>
2023-10-23 15:52:44 +0300

    The second commit

--- task4/src/ru/croc/winter/Main.java
+++ task4/src/ru/croc/winter/Main.java
@@ -7,32 +7,26 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner1 = new Scanner(System.in);
-        System.out.print("Enter a start: ");
-        if (scanner1.hasNextInt()) {
-            int a_start = scanner1.nextInt();}
-        else {
-            System.out.print("Incorrect input")
-        }
+        System.out.print("Enter a start number, a progression step and number of members: ");
 
         Scanner scanner2 = new Scanner(System.in);
-        System.out.print("Enter a progression step : ");
-        if (scanner2.hasNextInt()) {
-            int d = scanner2.nextInt();}
-        else {
-            System.out.print("Incorrect input")
-        }
 
         Scanner scanner3 = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        if (scanner3.hasNextInt()) {
-            int number = scanner3.nextInt();}
-        else {
-            System.out.print("Incorrect input")
+
+
+        if (scanner1.hasNextInt() && scanner2.hasNextInt() && scanner3.hasNextInt()) {
+            int a_start = scanner1.nextInt();
+            int d = scanner2.nextInt();
+            int number = scanner3.nextInt();
+            int summa = 0;
+
+            for (int i = 0; i < number; i++) {
+                summa += a_start + d * i;
+            }
+            System.out.println("Result = " + summa);
         }
-
-
-        for
-
-
+        else {
+            System.out.print("Incorrect input");
+        }
     }
 }
